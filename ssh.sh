f3076bd60d889a4bcfac5708ad9dca2ad16a675a
JanneAl <janne.alhonen@hotmail.com>
2017-11-03 16:31:46 +0200

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,24 @@
+echo "***************************"
+echo " "
+echo "Hei $USER"
+echo " "
+echo "***************************"
+sudo timedatectl set-timezone Europe/Helsinki
+setxkbmap fi
+sudo apt update
+sudo apt install -y git tree puppet
+
+
+cd /etc/puppet/modules
+
+sudo git clone https://github.com/JanneAl/sshfast
+
+sudo puppet apply -e 'class{"sshd":}'
+
+cd
+
+echo "***************************"
+echo " "
+echo "Valmiina käyttöön"
+echo " "
+echo "***************************"
